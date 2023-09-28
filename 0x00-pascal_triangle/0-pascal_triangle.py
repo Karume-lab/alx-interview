cd08f21b04fa1b1068d47f86d6c45fd40a548edb
Karume-lab <danielkarume@outlook.com>
2023-09-28 20:38:50 +0300

    pascal

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,15 @@
+def pascal_triangle(n):
+	num_list = []
+	if n <= 0:
+		return num_list
+	
+	result = [1]
+	num_list.append(result)
+	for i in range(1, n):
+		temp = [0] + result + [0]
+		result = []
+		for j in range(len(temp) - 1):
+			result.append(temp[j] + temp[j + 1])
+		num_list.append(result)
+
+	return num_list
