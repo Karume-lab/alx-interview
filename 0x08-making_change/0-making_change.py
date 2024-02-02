abd16e8e4c2188554ec6d9eed6c7c6a05a62f89a
Karume-lab <danielkarume@outlook.com>
2024-02-02 13:36:12 +0300

    making change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,22 +1,18 @@
 #!/usr/bin/python3
-"""Making change"""
+"""
+Chamge comes from within
+"""
 
 
 def makeChange(coins, total):
-    """Function that makes change from given coins"""
-    if total is 0 or total < 0:
+    """
+    Given a pile of cons of different values,
+    determine the fewest number of cons needed to meet a given amount total.
+    """
+    if total <= 0:
         return 0
-    if coins == [] or coins is None:
-        return -1
-    remainder = total
-    coins_count = 0
-    coin_id = 0
-    sorted_coins = sorted(coins, reverse=True)
-    n = len(coins)
-    while remainder > 0:
-        if remainder - sorted_coins[coin_id] >= 0:
-            remainder -= sorted_coins[coin_id]
-            coins_count += 1
-        else:
-            coin_id += 1
-    return coins_count
+    dp = [0] + [float("inf")] * (total)
+    for coin in coins:
+        for i in range(coin, total + 1):
+            dp[i] = min(dp[i], dp[i - coin] + 1)
+    return dp[-1] if dp[-1] != float("inf") else -1
